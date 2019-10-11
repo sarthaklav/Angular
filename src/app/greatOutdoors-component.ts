a761ebcb0cde60d3ce3d3bd92ed25fd59098c8c2
Sarthak Lav <sarthakchaudhary52@gmail.com>
2019-10-11 20:57:54 +0530

    Create greatOutdoors-component.ts

---
+++ src/app/greatOutdoors-component.ts
@@ -0,0 +1,16 @@
+import { FormGroup, ValidationErrors } from '@angular/forms';
+
+export class GreatOutdoorsComponentBase {
+  getFormGroupErrors(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(key => {
+      const controlErrors: ValidationErrors = formGroup.get(key).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+        });
+      }
+    });
+  }
+}
+
+

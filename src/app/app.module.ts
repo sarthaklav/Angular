684e20342b1b7c2e34fabb472320f0114d02c99b
Sarthak Lav <sarthakchaudhary52@gmail.com>
2019-10-11 20:57:26 +0530

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment.prod';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+
+import { GreatOutdoorsDataService } from './InMemoryWebAPIServices/greatOutdoors-data.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+   LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    environment.production ? HttpClientInMemoryWebApiModule.forRoot(GreatOutdoorsDataService, { delay: 1000 }) : [],
+    //RetailerModule
+    //import { RetailerProfileComponent } from './RetailerModule/retailer-profile/retailer-profile.component';
+    //import { RetailerModule } from './RetailerModule/retailer.module';
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+

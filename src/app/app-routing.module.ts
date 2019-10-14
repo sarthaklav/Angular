fef74c05b2a3568558a94c0e8bc10d9ad4dc951c
Sarthak Lav <sarthakchaudhary52@gmail.com>
2019-10-14 18:29:05 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   
-  //{ path: "retailer", loadChildren: () => import("./RetailerModule/retailer.module").then(m => m.RetailerModule) },
+  { path: "retailer", loadChildren: () => import("./RetailerModule/retailer.module").then(m => m.RetailerModule) },
   { path: "**", redirectTo: '/login', pathMatch: 'full' },
 ];
 

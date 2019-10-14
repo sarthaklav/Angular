d486e040b5f61ed7e3e992fe8bbde5d26c954601
Sarthak Lav <sarthakchaudhary52@gmail.com>
2019-10-14 18:29:55 +0530

    Update retailer.module.ts

--- src/app/RetailerModule/retailer.module.ts
+++ src/app/RetailerModule/retailer.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { SalesPersonsComponent } from './SalesPersons/salespersons.component';
-import { ProductsComponent } from './Products/products.component';
-import { AdminRoutingModule } from './admin-routing.module';
+import { RetailerHomeComponent } from './retailer-home/retailer-home.component';
+import { RetailerRoutingModule } from './retailer-routing.module';
 
 @NgModule({
   declarations: [
-    AdminHomeComponent,
-    SalesPersonsComponent,
-    ProductsComponent
+    RetailerHomeComponent
+
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    AdminRoutingModule
+    RetailerRoutingModule
   ],
   exports: [
-    AdminRoutingModule,
-    AdminHomeComponent,
-    SalesPersonsComponent,
-    ProductsComponent
+    RetailerRoutingModule,
+    RetailerHomeComponent
   ]
 })
-export class AdminModule { }
-
+export class RetailerModule { }
